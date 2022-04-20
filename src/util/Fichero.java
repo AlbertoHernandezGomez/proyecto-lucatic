e2c16b59f905f3930347cf8fada2c2e85f4e32fa
Roberto Negro <rhonegro@gmail.com>
2022-04-20 12:17:00 +0200

    clase DAO modificada

---
+++ src/util/Fichero.java
@@ -0,0 +1,17 @@
+package util;
+
+import java.util.ArrayList;
+
+import model.Videojuego;
+
+public class Fichero {
+	
+	public ArrayList<Videojuego> leerFichero(){
+		return null;
+	}
+	
+	public void escribirFichero(Videojuego videojuego) {
+		
+	}
+
+}
