0969216ff7565f4f280d83d640e1506c7a0422d4
AlbertoHernandezGomez <94531866+AlbertoHernandezGomez@users.noreply.github.com>
2022-04-20 14:02:43 +0200

    Add files via upload

--- src/services/VideojuegoService.java
+++ src/services/VideojuegoService.java
@@ -1,14 +1,37 @@
 package services;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
-import interfaces.IVideojuego;
+import interfaces.IVideojuegoService;
 import model.Videojuego;
 
-public class VideojuegoService implements IVideojuego {
+public class VideojuegoService implements IVideojuegoService {
 
-	public void dar_alta_videojuego(Videojuego videojuego) {
-		// TODO Auto-generated method stub
+	
+	@Override
+	public void darDeAltaVideojuego() {
+		
+		//ESCANER PARA LEER POR PANTALLA
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Introduzca el nombre del videojuego: ");
+		String nombre_videojuego = sc.next();
+		System.out.println("Introduzca la plataforma: ");
+		String plataforma = sc.next();
+		System.out.println("Introduza el año de lanzamiento: ");
+		int anyo_lanzamiento = sc.nextInt();
+		System.out.println("Introduzca el genero: ");
+		String genero = sc.next();
+		System.out.println("Introduzca el nombre de la compañia: ");
+		String publisher = sc.next();
+	
+		
+		
+		//IVideojuegoDAO videojuegodao = metodo.obtenerCSV
+		//videojuegodao.addVideojuego(videojuego)
+		//cerrarfichero();
+
 
 	}
 
