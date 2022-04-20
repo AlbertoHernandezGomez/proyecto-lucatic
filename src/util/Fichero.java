edfef839c86cf49d39756d5288e7dc4b2c04af87
Alberto Hernández Gómez <he_alberto@hotmail.com>
2022-04-20 10:49:21 +0200

    commit para probar

---
+++ src/util/Fichero.java
@@ -0,0 +1,16 @@
+package util;
+
+import java.util.ArrayList;
+
+import model.Videojuego;
+
+public class Fichero {
+	
+	public ArrayList<Videojuego> leerFichero(){
+		return null;
+	}
+	
+	public void escribirFichero(Videojuego videojuego) {
+		
+	}
+}
